af61c60f2d19e3c85f93642ce4a3af755740f567
Annika Ahlström <annikaahl92@gmail.com>
2025-10-01 11:02:28 +0200

    test number of bottles

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+//Uppgifter
+/* Annika Ahlström
+2025-10-01
+annikaahl92@gmail.com
+ */
+
 console.log("Hello World!");
 console.log("It's a sunny day!"); console.log("Not") ;
 console.log("Tomorrow it will be sunny!");
+// Detta är ett test
+/*
+console.log("Look at the weather app");
+  */
+// Uppgifter
+
+const exampleString = "Hello again!";
+console.log(exampleString);
+
+let numberofbottles = 10
+console.log(numberofbottles);
+const line1 = "Green bottles hanging on the wall";
+console.log(line1);
+let numberoflessbottles = 9
+const line2 = "and if one green bottle, should accidentally fall, there will be";
+console.log(line2);
+console.log (numberoflessbottles);
+
